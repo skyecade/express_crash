a55279ed97e945857eb4c364f7f11414498ca37b
Robert Sinclaire <robert@robertsinclaire.com>
2021-04-27 19:49:39 -0400

    22:02: learning routes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-// Bring in express
+// Bring in express and other node modules
 const express = require('express');
+const path = require('path');
 
 // Intialize instance of express called app
 const app = express();
 
 // Set a route
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
+app.get('/test', (req, res) => {
+    res.send('<h1>TEST</h1>');
 });
+
+app.get('/about', (req, res) => {
+    res.send('<h1>This is all about me</h1>');
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set port number to be used (system assigned PORT or 5000)
 const PORT = process.env.PORT || 5000;
