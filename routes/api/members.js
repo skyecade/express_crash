35b4dec5a2ef94b95f778e7913f8f427de62b280
Robert Sinclaire <robert@robertsinclaire.com>
2022-02-20 19:38:14 -0500

    finished CRUD app

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const res = require('express/lib/response');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members');
 
@@ -18,7 +20,50 @@ router.get('/:id', (req,res) => {
 
 // Create Member
 router.post('/', (req, res) => {
-    res.send(req.body);
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    if(!newMember.name || !newMember.email) {
+        return res.status(400).json({ msg: 'Please include a name and email'});
+    }
+
+    members.push(newMember);
+    res.json(members);
+});
+
+// Udate Member
+router.put('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updMember = req.body;
+        members.forEach(member => {
+            if(member.id === parseInt(req.params.id)) {
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+
+                res.json({ msg: 'Member udated', member });
+            }
+        });
+
+    } else {
+        res.status(400).json({ msg: `No member ID ${req.params.id} found.`});
+}
+});
+
+//Delete Member
+router.delete('/:id', (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+    res.json({ msg: 'Member deleted', members: members.filter(member => member.id !== parseInt(req.params.id))});
+} else {
+    res.status(400).json({ msg: `No member ID ${req.params.id} found.`});
+}
 });
 
 module.exports = router;
